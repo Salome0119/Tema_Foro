7cef074f2403b444df0b39089926e72312371a30
Salome0119 <salohenao19@gmail.com>
2026-05-12 06:47:03 -0500

    Mejora del archivo forms.py

--- inventario_django/dcrm/website/forms.py
+++ inventario_django/dcrm/website/forms.py
@@ -1,4 +1,22 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Record
+from .models import Perfil
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("El correo ya está registrado")
+        return email
+
+class PerfilForm(forms.ModelForm):
+    class Meta:
+        model = Perfil
+        fields = ['telefono', 'direccion']
